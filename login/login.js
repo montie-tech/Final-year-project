a7eb10019fcca2220f0e0d2c8c66139f316a113b
Elyees Tatua <elyeestatua@gmail.com>
2024-01-27 02:08:51 +0300

    ops(login): Working on the login.js file

---
+++ login/login.js
@@ -0,0 +1,8 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Add your login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+}
